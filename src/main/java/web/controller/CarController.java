54f6077304e7e0c2e724f0f15bd619b0621d9faa
verdan <job.mail1358@gmail.com>
2021-01-08 21:43:58 +0600

    нашел еще ошибку исправил

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import service.CarService;
-import service.CarServiceImpl;
+import web.service.CarService;
+import web.service.CarServiceImpl;
 
 @Controller
 public class CarController {
@@ -13,7 +13,7 @@ public class CarController {
     private final CarService carService = new CarServiceImpl();
 
     @GetMapping(value = "/cars")
-    public String printCars(@RequestParam(required = false) int count, ModelMap model) {
+    public String printCars(@RequestParam(required = false) Integer count, ModelMap model) {
         model.addAttribute("messages", carService.getCar(count));
         return "Car";
     }
